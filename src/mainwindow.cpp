c123dabbcc9bde0e19be74205b54d98d32372ad7
YutingLi <liyuting.xa@163.com>
2026-06-17 11:20:35 +0800

    修改mainwindow，closeAction trigger QCloseEvent

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -7,7 +7,17 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    // bind connections
+    connect(this, &MainWindow::sigStopThread, &m_diceThread, &ThreadDice::slotStopThread);
+    connect(this, &MainWindow::sigStartDice, &m_diceThread, &ThreadDice::slotStartDice);
+    connect(this, &MainWindow::sigPauseDice, &m_diceThread, &ThreadDice::slotPauseDice);
+    connect(&m_diceThread, &ThreadDice::sigDiceValueChanged, this, &MainWindow::slotDiceValueChanged);
 
+    // action button state management
+    ui->actionstartThread->setEnabled(true);
+    ui->actionStopThread->setEnabled(false);
+    ui->actionstartDice->setEnabled(false);
+    ui->actionPauseDice->setEnabled(false);
 }
 
 MainWindow::~MainWindow()
@@ -17,36 +27,73 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_actionstartThread_triggered()
 {
-    qDebug() << "...";
+    m_diceThread.start();
+    ui->actionstartThread->setEnabled(false);
+    ui->actionStopThread->setEnabled(true);
+    ui->actionstartDice->setEnabled(true);
+    ui->actionPauseDice->setEnabled(false);
 }
-
-
-void MainWindow::on_actionstartDice_triggered()
-{
-
-}
-
-
-void MainWindow::on_actionClose_triggered()
-{
-
-}
-
-
-void MainWindow::on_actionClearText_triggered()
-{
-
-}
-
 
 void MainWindow::on_actionStopThread_triggered()
 {
-
+    if (m_diceThread.isRunning()) {
+        emit sigStopThread();
+    }
+    ui->actionStopThread->setEnabled(false);
+    ui->actionstartDice->setEnabled(false);
+    ui->actionPauseDice->setEnabled(false);
+    ui->actionstartThread->setEnabled(true);
 }
 
+void MainWindow::on_actionstartDice_triggered()
+{
+    if (m_diceThread.isRunning()) {
+        emit sigStartDice();
+    }
+    ui->actionstartDice->setEnabled(false);
+    ui->actionPauseDice->setEnabled(true);
+}
 
 void MainWindow::on_actionPauseDice_triggered()
 {
-
+    emit sigPauseDice();
+    ui->actionstartDice->setEnabled(true);
+    ui->actionPauseDice->setEnabled(false);
 }
 
+void MainWindow::closeEvent(QCloseEvent* event) {
+    if (m_diceThread.isRunning()) {
+        emit sigStopThread();
+    }
+    bool exited = m_diceThread.wait(500);
+    if (!exited) {
+        m_diceThread.terminate();
+    }
+    event->accept();
+}
+
+void MainWindow::on_actionClearText_triggered()
+{
+    m_showText.clear();
+    ui->plainTextEdit->clear();
+}
+
+void MainWindow::slotDiceValueChanged(quint8 diceVal) {
+    // 是否越界
+    if (diceVal < 1 || diceVal > 6) return;
+    // 显示
+    displayDiceText(diceVal);
+    displayDicePixmap(diceVal);
+}
+
+void MainWindow::displayDiceText(quint8 diceVal) {
+    QString curText = QString("第 n 次掷骰子，点数为：%1\n").arg(diceVal);
+    m_showText.append(curText);
+    ui->plainTextEdit->setPlainText(m_showText);
+}
+
+void MainWindow::displayDicePixmap(quint8 diceVal) {
+    QString pixFileName = QString(":/dice/images/d%1.jpg").arg(diceVal);
+    QPixmap pixmap(pixFileName);
+    ui->lblPic->setPixmap(pixmap);
+}
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "threaddice.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,21 +17,27 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
+protected:
+    void closeEvent(QCloseEvent* event) override;
 
 private slots:
+    void slotDiceValueChanged(quint8 diceVal);
     void on_actionstartThread_triggered();
-
     void on_actionstartDice_triggered();
-
-    void on_actionClose_triggered();
-
     void on_actionClearText_triggered();
-
     void on_actionStopThread_triggered();
-
     void on_actionPauseDice_triggered();
 
+signals:
+    void sigStopThread();
+    void sigStartDice();
+    void sigPauseDice();
+
 private:
+    void displayDiceText(quint8 diceVal);
+    void displayDicePixmap(quint8 diceVal);
     Ui::MainWindow *ui;
+    ThreadDice m_diceThread;
+    QString m_showText;
 };
 #endif // MAINWINDOW_H
